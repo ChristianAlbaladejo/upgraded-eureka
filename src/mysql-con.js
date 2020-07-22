b0a97b6a04db153ad355f8956b2c8674a6c84a15
ChristianAlbaldejo <christian.albaladejo06@gmail.com>
2020-07-22 11:58:03 +0200

    check server

---
+++ src/mysql-con.js
@@ -0,0 +1,43 @@
+/* 'use strict'
+const mysql = require('mysql');
+ */
+/*  const mysqlConnection = mysql.createConnection({
+  host: 'eu-cdbr-west-03.cleardb.net',
+  user: 'b0ab591da45cbb',
+  password: 'bdbc7002',
+  database: 'heroku_2205e3ccffad011',
+  multipleStatements: true
+});  */
+/* const mysqlConnection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'sistema',
+}); */
+//mysql://b0ab591da45cbb:bdbc7002@eu-cdbr-west-03.cleardb.net/heroku_2205e3ccffad011?reconnect=true
+/* mysqlConnection.connect(function (err) {
+  if (err) {
+    console.error(err);
+    return;
+  } else {
+    console.log('db is connected');
+  }
+}); */
+'use strict';
+var mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'eu-cdbr-west-03.cleardb.net',
+  user: 'b0ab591da45cbb',
+  password: 'bdbc7002',
+  database: 'heroku_2205e3ccffad011',
+});
+
+connection.connect(function (error) {
+  if (!!error) {
+    console.log(error);
+  } else {
+    console.log('Connected!:)');
+  }
+});
+
+module.exports = connection;
