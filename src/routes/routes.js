355d8571d5b1f2344bf3c0aae525b9a89ff53e6a
ChristianAlbaldejo <christian.albaladejo06@gmail.com>
2020-08-07 11:44:56 +0200

    login

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -67,7 +67,8 @@ router.get('/familiName/:id', (req, res) => {
 
 // INSERT a order
 router.post('/order', md_auth.ensureAuth, (req, res) => {
-  let now = new Date.now();
+  const now = new Date.now();
+  console.log(now)
   if (req.body) {
     var post = req.body;
     console.log(req.body);
