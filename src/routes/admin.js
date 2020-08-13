a9e2262c1aadb35ea3745319e52421458c39c728
ChristianAlbaldejo <christian.albaladejo06@gmail.com>
2020-08-13 13:27:32 +0200

    updates

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -87,6 +87,7 @@ router.get('/admin/returnUser/:id', md_auth.ensureAuth, (req, res) => {
 
 router.put('/admin/updateOrder/:order', md_auth.ensureAuth, (req, res) => {
     const { order } = req.params;
+    console.log(JSON.stringify(req.headers));
     mysqlConnection.query("UPDATE salesorder set sended = 'true' WHERE id=?", [order], (err, rows, fields) => {
         if (!err) {
             res.json(rows);
