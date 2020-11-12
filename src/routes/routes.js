b4c5c3551b867455fe4d67a1fff938cf3594fa04
ChristianAlbaldejo <christian.albaladejo06@gmail.com>
2020-11-12 17:50:47 +0100

    addFamily

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -823,25 +823,16 @@ router.post('/resetpassword', function (req, res) {
   res.send('Your password has been successfully changed.');
 });
 
-router.post('/create-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-          },
-          unit_amount: 2000,
-        },
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: `${YOUR_DOMAIN}/success.html`,
-    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+router.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 100,
+    currency: "usd"
   });
-  res.json({ id: session.id });
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  });
 });
 
 /**
